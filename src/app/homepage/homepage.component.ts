d386f910651e7d20440a5cec0564eebf4ce24836
RithyTep <teprithy2020@gmail.com>
2024-03-02 14:09:20 +0700

    t

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -5,111 +5,116 @@ import { MatDialog } from '@angular/material/dialog';
 import { CountryStore } from '../Stores/country.store';
 
 @Component({
- selector: 'app-homepage',
- templateUrl: './homepage.component.html',
- styleUrls: ['./homepage.component.scss'],
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent {
- country: any[] = [];
- currentPage = 1;
- totalPages: number[] = [];
- itemsPerPage: number = 25;
+  country: any[] = [];
+  currentPage = 1;
+  totalPages: number[] = [];
+  itemsPerPage: number = 25;
 
- constructor(
+  constructor(
     private api: ApiService,
     public dialog: MatDialog,
     public store: CountryStore
- ) {}
+  ) {}
 
- isMenuOpen = false;
- isLoading = false;
+  isMenuOpen = false;
+  isLoading = false;
 
- ngOnInit() {
+  ngOnInit() {
     this.loadCountries();
- }
+  }
 
- loadCountries() {
+  loadCountries() {
     this.isLoading = true;
     this.api.fetchCountry().subscribe((data: any) => {
       this.country = data;
       this.calculateTotalPages();
       this.isLoading = false;
     });
- }
+  }
 
- toggleMenu() {
+  toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
- }
+  }
 
- get currentPageItems() {
+  get currentPageItems() {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
     return this.filteredCountries.slice(startIndex, endIndex);
- }
+  }
 
- calculateTotalPages() {
+  calculateTotalPages() {
     this.totalPages = [
-      ...Array(Math.ceil(this.filteredCountries.length / this.itemsPerPage)).keys(),
+      ...Array(
+        Math.ceil(this.filteredCountries.length / this.itemsPerPage)
+      ).keys(),
     ].map((i) => i + 1);
- }
+  }
 
- navigatePage(direction: 'next' | 'prev') {
-    const totalPages = Math.ceil(this.filteredCountries.length / this.itemsPerPage);
+  navigatePage(direction: 'next' | 'prev') {
+    const totalPages = Math.ceil(
+      this.filteredCountries.length / this.itemsPerPage
+    );
     if (direction === 'next' && this.currentPage < totalPages) {
       this.currentPage++;
     } else if (direction === 'prev' && this.currentPage > 1) {
       this.currentPage--;
     }
- }
+  }
 
- goToPage(pageNumber: number) {
+  goToPage(pageNumber: number) {
     this.currentPage = pageNumber;
- }
+  }
 
+  get filteredCountries() {
+    let filteredCountries;
+    if (this.store.searchTerm) {
+      filteredCountries = this.country.filter((item) =>
+        item.name.official
+          .toLowerCase()
+          .includes(this.store.searchTerm.toLowerCase())
+      );
+    } else {
+      filteredCountries = this.country;
+    }
+    this.totalPages = Array(
+      Math.ceil(filteredCountries.length / this.itemsPerPage)
+    )
+      .fill(0)
+      .map((x, i) => i + 1);
 
- get filteredCountries() {
-   let filteredCountries;
-   if (this.store.searchTerm) {
-     filteredCountries = this.country.filter((item) =>
-       item.name.official
-         .toLowerCase()
-         .includes(this.store.searchTerm.toLowerCase())
-     );
-   } else {
-     filteredCountries = this.country;
-   }
-   this.totalPages = Array(
-     Math.ceil(filteredCountries.length / this.itemsPerPage)
-   )
-     .fill(0)
-     .map((x, i) => i + 1);
+    if (this.currentPage > this.totalPages.length) {
+      this.currentPage = 1;
+    }
 
-     if (this.currentPage > this.totalPages.length) {
-     this.currentPage = 1;
-   }
+    return filteredCountries;
+  }
 
-   return filteredCountries;
- }
-
- get visiblePages() {
+  get visiblePages() {
     const start = Math.max(0, this.currentPage - 2);
     const end = Math.min(this.totalPages.length, start + 4);
     return this.totalPages.slice(start, end);
- }
+  }
 
- sortCountries(order: string) {
+  sortCountries(order: string) {
     this.isMenuOpen = false;
     this.country.sort((a, b) => {
       const nameA = a.name.official.toLowerCase();
       const nameB = b.name.official.toLowerCase();
-      return order === 'asc' ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA);
+      return order === 'asc'
+        ? nameA.localeCompare(nameB)
+        : nameB.localeCompare(nameA);
     });
- }
+  }
 
- openDialog(item: any) {
+  openDialog(item: any) {
     this.dialog.open(CountryDetailDialogComponent, {
       width: '1400px',
       data: item,
     });
- }
+  }
 }
